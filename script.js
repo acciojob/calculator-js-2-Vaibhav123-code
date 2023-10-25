31c31f9cceed884c51326ab439157b8868d1df6e
Vaibhav123-code <124595249+Vaibhav123-code@users.noreply.github.com>
2023-10-25 19:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btns button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const buttonText = this.textContent;
+        if (buttonText === 'C') {
+            display.value = '';
+        } else if (buttonText === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else {
+            display.value += buttonText;
+        }
+    });
+});
